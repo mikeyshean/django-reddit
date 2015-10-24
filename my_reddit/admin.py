58b942983f03930bd743fb3b2ccc5198c48ed5c5
Mikey Shean <mdshean2@gmail.com>
2015-10-24 14:08:51 -0400

    Add post inline to admin.

--- my_reddit/admin.py
+++ my_reddit/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import User, Sub, Post
 
 admin.site.register(User)
-admin.site.register(Sub)
-admin.site.register(Post)
+
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 1
+
+class SubAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields': ['name']}),
+        ('Description', {'fields': ['description']}),
+        ('Author', {'fields': ['creator']}),
+    ]
+    inlines = [PostInline]
+
+admin.site.register(Sub, SubAdmin)
