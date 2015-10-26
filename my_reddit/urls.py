2ac449d3a96d00a6787a2b298970997e702ff325
Mikey Shean <mdshean2@gmail.com>
2015-10-26 12:37:13 -0400

    Add NewSub view/template.

--- my_reddit/urls.py
+++ my_reddit/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'new/', views.new_sub, name='new_sub'),
     url(r'^(?P<pk>[0-9]+)/$', views.SubView.as_view(), name='sub'),
     url(r'^(?P<sub_id>[0-9]+)/post/(?P<post_id>[0-9]+)/$', views.post_view, name='post'),
     url(r'^(?P<sub_id>[0-9]+)/post/(?P<post_id>[0-9]+)/comment/$', views.parent_comment, name='parent_comment'),
--- my_reddit/views.py
+++ my_reddit/views.py
@@ -5,11 +5,20 @@ from django.views import generic
 from .models import User, Sub, Post, Comment
 
 class IndexView(generic.ListView):
-    template_name = 'my_reddit/index.html'
-    context_object_name = 'subs_list'
 
-    def get_queryset(self):
-        return Sub.objects.order_by('name')
+    def get(self, request):
+        template_name = 'my_reddit/index.html'
+        subs_list = Sub.objects.order_by('name')
+        return render(request, template_name, { 'subs_list': subs_list })
+
+    def post(self, request):
+        name = request.POST['name']
+        description = request.POST['description']
+        sub = Sub.objects.create(name=name, description=description, creator_id=1)
+        return HttpResponseRedirect(reverse('my_reddit:sub', args=(sub.id,)))
+
+def new_sub(request):
+    return render(request, 'my_reddit/new_sub.html')
 
 class SubView(generic.DetailView):
     template_name = 'my_reddit/sub_show.html'
@@ -41,7 +50,7 @@ class CommentView(generic.DetailView):
 
 def post_view(request, sub_id, post_id):
     ## Work on pre-fetching nested comments:
-    
+
     # top_level_comments = Comment.objects.filter(parent_id=null)
     # post = Post.objects.prefetch_related(
     #     Prefetch(
