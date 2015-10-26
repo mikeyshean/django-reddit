f82797b7aefe327b61028b08c2a27392394fbbea
Mikey Shean <mdshean2@gmail.com>
2015-10-26 13:16:44 -0400

    Add NewPost view/template.

--- my_reddit/urls.py
+++ my_reddit/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'new/', views.new_sub, name='new_sub'),
+    url(r'^new/', views.new_sub, name='new_sub'),
     url(r'^(?P<pk>[0-9]+)/$', views.SubView.as_view(), name='sub'),
+    url(r'^(?P<sub_id>[0-9]+)/new/$', views.new_post, name='new_post'),
     url(r'^(?P<sub_id>[0-9]+)/post/(?P<post_id>[0-9]+)/$', views.post_view, name='post'),
     url(r'^(?P<sub_id>[0-9]+)/post/(?P<post_id>[0-9]+)/comment/$', views.parent_comment, name='parent_comment'),
     url(r'^comments/(?P<pk>[0-9]+)/$', views.CommentView.as_view(), name='comment'),
--- my_reddit/views.py
+++ my_reddit/views.py
@@ -18,13 +18,45 @@ class IndexView(generic.ListView):
         return HttpResponseRedirect(reverse('my_reddit:sub', args=(sub.id,)))
 
 def new_sub(request):
+    print "SUB"
     return render(request, 'my_reddit/new_sub.html')
 
 class SubView(generic.DetailView):
-    template_name = 'my_reddit/sub_show.html'
 
-    def get_queryset(self):
-        return Sub.objects.prefetch_related("posts__author")
+    def get(self, request, *args, **kwargs):
+        template_name = 'my_reddit/sub_show.html'
+        sub = get_object_or_404(
+            Sub.objects.prefetch_related("posts__author"),
+            pk=kwargs['pk']
+        )
+
+        return render(request, template_name, { 'sub': sub })
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST['title']
+        text = request.POST['text']
+        sub_id = request.POST['sub_id']
+
+        post = Post.objects.create(title=title, text=text, sub_id=sub_id, author_id=1)
+        return HttpResponseRedirect(reverse('my_reddit:post', args=(sub_id,post.id)))
+
+def new_post(request, sub_id):
+    print "YOLO"
+    return render(request, 'my_reddit/new_post.html', { 'sub_id': sub_id })
+
+def post_view(request, sub_id, post_id):
+    ## Work on pre-fetching nested comments:
+
+    # top_level_comments = Comment.objects.filter(parent_id=null)
+    # post = Post.objects.prefetch_related(
+    #     Prefetch(
+    #         "comments__author",
+    #         queryset=top_level_comments,
+    #         to_attr='top_comments'
+    #     )).get(pk=post_id)
+
+    post = Post.objects.prefetch_related("comments__author").get(pk=post_id)
+    return render(request, 'my_reddit/post_show.html', { 'post': post })
 
 class CommentView(generic.DetailView):
 
@@ -47,20 +79,6 @@ class CommentView(generic.DetailView):
 
         return HttpResponseRedirect(reverse('my_reddit:comment', args=(parent.id,)))
 
-
-def post_view(request, sub_id, post_id):
-    ## Work on pre-fetching nested comments:
-
-    # top_level_comments = Comment.objects.filter(parent_id=null)
-    # post = Post.objects.prefetch_related(
-    #     Prefetch(
-    #         "comments__author",
-    #         queryset=top_level_comments,
-    #         to_attr='top_comments'
-    #     )).get(pk=post_id)
-
-    post = Post.objects.prefetch_related("comments__author").get(pk=post_id)
-    return render(request, 'my_reddit/post_show.html', { 'post': post })
 
 def parent_comment(request, sub_id, post_id):
     post = get_object_or_404(Post, pk=post_id)
