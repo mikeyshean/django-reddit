ed5b64da4290637d4697cb2f0698d748cb7beaa7
Mikey Shean <mdshean2@gmail.com>
2015-10-29 23:59:46 -0400

    Deprecate staticfile js loading.

--- my_reddit/static/js/views/subs_index.js
+++ my_reddit/static/js/views/subs_index.js
@@ -2,7 +2,6 @@ MyReddit.Views.SubsIndex = Backbone.View.extend({
   template: JST.subs_index,
 
   render: function () {
-    console.log(this.collection);
     this.$el.html(this.template({ subs: this.collection }));
     return this;
   }
